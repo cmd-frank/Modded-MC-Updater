7db0cb05311f50f32aea46548997eb63491a1510
iFrank <iFrank@FRANK>
2022-04-04 02:48:59 -0700

    Changed assembly information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Update Server SSH")]
+[assembly: AssemblyTitle("Update Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Update Server SSH")]
+[assembly: AssemblyCompany("fun-Frank")]
+[assembly: AssemblyProduct("Update Server")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
